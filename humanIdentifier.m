0971dd023fd24b3397dc2d29c36543878368f2da
AtiahY <83597746+AtiahY@users.noreply.github.com>
2021-06-01 20:35:08 +0600

    Update humanIdentifier.m

--- humanIdentifier.m
+++ humanIdentifier.m
@@ -1,4 +1,23 @@
+clc;
+clear all;
+close all;
 
-function [coordinate] = humanIdentifier(img)
-% code here
+vid = vision.VideoFileReader('vtest.avi');
+vid_play = vision.VideoPlayer('Position',[300 100 1000 500]);
+
+detector = peopleDetectorACF();
+
+while ~isDone(vid)
+    frame = step(vid);
+    I=double(frame);
+    [bboxes,scores] = detect(detector,I);
+    
+    
+    if ~isempty(bboxes)
+         I= insertObjectAnnotation(I,'rectangle',bboxes,scores,'color','g');
+    end
+    step(vid_play,I);
 end
+
+release(vid);
+release(vid_play);
